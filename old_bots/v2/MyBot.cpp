74451de8dccced2396580ac1799ba7918a13459f
uvafan <uvafan173@gmail.com>
2017-12-01 18:47:42 -0500

    archived shitty suiciding v2, will improve later

---
+++ old_bots/v2/MyBot.cpp
@@ -0,0 +1,43 @@
+#include "hlt/hlt.hpp"
+#include "hlt/navigation.hpp"
+#include "hlt/location.hpp"
+#include "djj/navigator.hpp"
+#include "djj/ship.hpp"
+#include "djj/macrocontroller.hpp"
+#include <map>
+#include <chrono>
+
+int main() {
+    const hlt::Metadata metadata = hlt::initialize("v2");
+    const hlt::PlayerId player_id = metadata.player_id;
+
+    const hlt::Map& initial_map = metadata.initial_map;
+
+    // We now have 1 full minute to analyse the initial map.
+    std::ostringstream initial_map_intelligence;
+    initial_map_intelligence
+        << "width: " << initial_map.map_width
+        << "; height: " << initial_map.map_height
+        << "; players: " << initial_map.ship_map.size()
+        << "; my ships: " << initial_map.ship_map.at(player_id).size()
+        << "; planets: " << initial_map.planets.size();
+    hlt::Log::log(initial_map_intelligence.str());
+    djj::Navigator nav = djj::Navigator::newNavigator(initial_map); 
+    djj::Macrocontroller mc = djj::Macrocontroller::newMacrocontroller(initial_map,player_id);
+    std::vector<hlt::Move> moves;
+    int turn = -1;
+    //int plans;
+    for (;;) {
+        //std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+        //plans = 0;
+        turn++;
+        const hlt::Map map = hlt::in::get_map();
+        mc.updateMapInfo(map,turn);
+        moves = mc.getMoves(turn);
+        
+        if (!hlt::out::send_moves(moves)) {
+            hlt::Log::log("send_moves failed; exiting");
+            break;
+        }
+    }
+}
---
+++ old_bots/v2/djj/ship.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "../hlt/location.hpp"
+#include "../hlt/move.hpp"
+#include "objective.hpp"
+#include <vector>
+#include <queue>
+
+namespace djj {
+    struct Ship {
+        int ID;
+        hlt::Location myLoc;
+        std::queue<hlt::Move> plan;
+        bool docked;
+        Objective obj;
+
+        static Ship makeShip(int myID, const hlt::Location& loc){
+            return {myID,loc,std::queue<hlt::Move>(),false,Objective::newObjective(ObjType::noop,loc,0,0,0)};
+        }
+
+        void setPlan(std::queue<hlt::Move> p){
+            plan = p;
+        }
+
+        void setDocked(bool d){
+            docked = d;
+        }
+
+        void setLocation(const hlt::Location& loc){
+            myLoc = loc;
+        }
+
+        void setObjective(Objective o){
+            obj = o;
+        }
+
+        bool operator <(const Ship& s) const{
+            return ID < s.ID;
+        }
+    };
+}
